86e1ec8b73e48c3242f9f633eada3578373dfc76
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2024-03-15 16:42:22 -0300

    Add files via upload

---
+++ UdemyCSharpPoo/Exercise3/Ex002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Ex002 {
+    class Progam {
+        static void Main(string[] args) {
+            
+            double rai = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            double area = rai * rai * pi;
+            Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+
+
+        }
+        
+    }
+}
+
+
