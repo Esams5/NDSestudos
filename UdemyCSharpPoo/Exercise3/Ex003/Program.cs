d3a53bb5656a09f01e693cc5c774291344f012fb
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2024-03-15 16:43:18 -0300

    Add files via upload

---
+++ UdemyCSharpPoo/Exercise3/Ex003/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex003
+{
+    class Pogram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 4 números: ");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D= int.Parse(Console.ReadLine());
+            int dif = (A * B) - (C * D);
+            
+            Console.WriteLine("DIFERENCA= " + dif);
+
+        }
+        
+    }
+}
+
