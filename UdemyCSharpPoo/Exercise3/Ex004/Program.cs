97341219eb372dacc8c5fc8952a0f4c4adc995f4
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2024-03-15 16:43:50 -0300

    Add files via upload

---
+++ UdemyCSharpPoo/Exercise3/Ex004/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex004
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+            double valor = double.Parse(Console.ReadLine());
+            double sal = valor * horas;
+            
+            Console.WriteLine("Number = " + num);
+            Console.WriteLine(("SALARY = " + sal.ToString("F2")));
+
+        }
+    }
+}
+
