9d7ca5f899069e433318d84593dd6f4b4d7cfb71
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2024-03-15 16:41:30 -0300

    Add files via upload

---
+++ UdemyCSharpPoo/Exercise3/Ex001/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Ex001 {
+    class Progam {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+
+            double soma = num1 + num2;
+            
+            Console.WriteLine("Soma = " + soma);
+
+
+
+
+        }
+    }
+}
