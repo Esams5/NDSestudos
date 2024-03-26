0bb99fd57114ee1faeb9394627a2772bb41e19ac
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2024-03-26 15:03:00 -0300

    Structs

---
+++ UdemyCSharpPoo/Exercise6/Estruturas/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Estruturas
+{
+    class Program
+    {
+        static void Main()
+        {
+            Point p;
+            p.X = 10;
+            p.Y = 20;
+            
+            
+            Console.WriteLine(p);
+
+            p = new Point();
+            Console.WriteLine(p);
+        }
+    }
+}
+
