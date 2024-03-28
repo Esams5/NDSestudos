c43b63d876ac907c8a3d4957c24d443981177509
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2024-03-28 15:36:09 -0300

    Exercício de fixação de Vetores

---
+++ UdemyCSharpPoo/Exercise6/Pensionato/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace Pensionato
+{
+    class Program
+    {
+        static void Main()
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                Console.WriteLine("Aluguel #" +i);
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                
+                vect[quarto] = new Estudante(nome, email);
+
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine( i +": " + vect[i]);
+                    
+                }
+                
+            }
+            
+            
+        }
+    }
+}
+
