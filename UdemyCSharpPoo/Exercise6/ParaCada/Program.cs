3cb478f0ec7480741d933d134143efaf7d4089a5
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2024-04-02 13:55:06 -0300

    Foreach

---
+++ UdemyCSharpPoo/Exercise6/ParaCada/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ParaCada
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            foreach (string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+            
+        }
+        
+
+    }
+}
+
