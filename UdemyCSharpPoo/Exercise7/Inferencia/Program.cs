d609103946ee0736168c47b59bb6b2a349a0032b
Samuel Esdras <118021278+Esams5@users.noreply.github.com>
2024-04-11 14:34:37 -0300

    Add files via upload

---
+++ UdemyCSharpPoo/Exercise7/Inferencia/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Inferencia
+{
+    class Program
+    {
+        static void Main()
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            var w = z; // é um erro
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+        
+    }
+}
