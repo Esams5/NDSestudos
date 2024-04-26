0b92c4ff5cc6d3fd497c3d786b01c49d20011768
Samuel Esdras <esdrasamuel57@gmail.com>
2024-04-26 17:37:08 -0300

    Enumerações parte 1

---
+++ UdemyCSharpPoo/Exercise8/Enum1/Entities/Order.cs
@@ -0,0 +1,16 @@
+using Enum1.Entities.Enums;
+
+namespace Enum1.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id + ", " + Moment + ", " + Status;
+        }
+    }
+}
---
+++ UdemyCSharpPoo/Exercise8/Enum1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Enum1.Entities;
+using Enum1.Entities.Enums;
+
+namespace Enum1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
+
